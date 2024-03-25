37d9a8cd47964f67cbc6336f9b0a58ad81223b2a
Manny Henri <mannyhenri@users.noreply.github.com>
2024-03-25 13:52:01 -0400

    Update PlayerForm.jsx

--- frontend/src/Components/Player/PlayerForm.jsx
+++ frontend/src/Components/Player/PlayerForm.jsx
@@ -2,11 +2,24 @@ import React from 'react'
 import axios from 'axios'
 
 class PlayerForm extends React.Component {
+    submitPlayer(event) {
+        event.preventDefault()
+
+        axios.post('http://localhost:4000/players', {
+            firstName: this.refs.firstName.value,
+            lastName: this.refs.lastName.value,
+            phone: this.refs.phone.value,
+            email: this.refs.email.value,
+        })
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error))
+    }
+
     render () {
         return (
             <div className='row'>
                 <h1 className='center'>Add a new player</h1>
-                <form className='col s12'>
+                <form className='col s12' onSubmit={this.submitPlayer.bind(this)}>
                     <div className='row'>
                         <div className='input-field col s6'>
                             <input id='firstName' ref='firstName' type='text' />
@@ -27,8 +40,7 @@ class PlayerForm extends React.Component {
                             <label htmlFor='email'>Email</label>
                         </div>
                     </div>
-                    <button className='btn waves-effect waves-light' type='submit'
-                    name='action'>Add Player</button>
+                    <button className='btn waves-effect waves-light' type='submit' name='action'>Add Player</button>
                 </form>
             </div>
         )
